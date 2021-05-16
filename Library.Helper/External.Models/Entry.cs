a9fe1f90feaab13a0ee618a839700530e966429f
Oscar McGovern O'Byrne <36077674+x173636253@users.noreply.github.com>
2021-05-16 18:57:09 +0100

    Add files via upload
    Part1

---
+++ Library.Helper/External.Models/Entry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Library.Helper.External.Models
+{
+
+    [Serializable]
+    [XmlRoot("Entry"), XmlType("Entry")]
+
+    public class Entry
+    {
+        public int RoleCode { get; set; }
+        public string RoleName { get; set; }
+        public string RoleAccess { get; set; }
+    }
+}
---
+++ Library.Helper/ReadXML.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data;
+using Library.Helper.External.Models;
+
+namespace Library.Helper
+{
+    public class ReadXML
+    {
+        public List<Entry> ReturnListOfRoles()
+        {
+            string XMLData = HttpContext.Current.Server.MapPath("~/App_Data/RankRoles.xml");
+
+            DataSet ds = new DataSet();
+
+            ds.ReadXml(XMLData);
+
+            var Roles = new List<Entry>();
+
+            Roles = (from rows in ds.Tables[0].AsEnumerable() 
+                     select new Entry
+                        {
+                            RoleCode = Convert.ToInt32(rows[0].ToString()),
+                            RoleName = rows[1].ToString(),
+                            RoleAccess = rows[2].ToString()
+                        }
+                     ).ToList();
+
+            return Roles;
+        }
+    }
+}
